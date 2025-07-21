51c08439356f58dd95c9028796f8d2f02c5cb33b
Frederick Wang <fredwang1012@gmail.com>
2025-07-21 13:33:29 -0700

    fixed conftest

--- tests/expectations/metrics/conftest.py
+++ tests/expectations/metrics/conftest.py
@@ -1,6 +1,8 @@
 from typing import Iterable, Optional, Union
+from unittest import mock
 
 import pytest
+from typing_extensions import override
 
 from great_expectations.compatibility.sqlalchemy import (
     sqlalchemy as sa,
@@ -10,14 +12,48 @@ from great_expectations.execution_engine import SqlAlchemyExecutionEngine
 from great_expectations.execution_engine.sqlalchemy_batch_data import SqlAlchemyBatchData
 
 
-class Dialect:
-    def __init__(self, dialect: str):
-        self.name = dialect
+def create_mock_dialect(dialect_name: str = "sqlite"):
+    """Create a mock SQLAlchemy dialect using MagicMock."""
+    mock_dialect = mock.MagicMock()  # noqa: TID251
+    mock_dialect.name = dialect_name
+
+    # Mock the statement compiler chain to return proper compiled SQL
+    def mock_statement_compiler(dialect, element, **kw):
+        """Mock statement compiler that returns appropriate SQL based on element type."""
+        mock_compiled = mock.MagicMock()  # noqa: TID251
+
+        # Determine what SQL to return based on the element type
+        if hasattr(element, "_is_subquery") and element._is_subquery:
+            # This is a subquery - return the expected subquery SQL
+            mock_compiled.__str__ = lambda self: "SELECT * \nFROM my_table"
+        elif hasattr(element, "get_final_froms"):
+            # This is a select statement - use the non-deprecated method
+            try:
+                final_froms = element.get_final_froms()
+                if final_froms:
+                    mock_compiled.__str__ = lambda self: "SELECT * \nFROM my_table"
+                else:
+                    mock_compiled.__str__ = lambda self: str(element)
+            except Exception:
+                # Fallback if get_final_froms() fails
+                mock_compiled.__str__ = lambda self: "SELECT * \nFROM my_table"
+        elif hasattr(element, "name"):
+            # This is a table - return the table name
+            mock_compiled.__str__ = lambda self: element.name
+        else:
+            # Fallback - try to get a reasonable string representation
+            mock_compiled.__str__ = lambda self: str(element)
+
+        return mock_compiled
+
+    mock_dialect.statement_compiler = mock_statement_compiler
+
+    return mock_dialect
 
 
 class MockSaEngine:
-    def __init__(self, dialect: Dialect):
-        self.dialect = dialect
+    def __init__(self, dialect_name: str = "sqlite"):
+        self.dialect = create_mock_dialect(dialect_name)
 
     def connect(self) -> None:
         pass
@@ -43,12 +79,13 @@ def batch_selectable() -> sa.Table:
 
 class MockSqlAlchemyExecutionEngine(SqlAlchemyExecutionEngine):
     def __init__(self, create_temp_table: bool = True, *args, **kwargs):
-        self.engine = MockSaEngine(dialect=Dialect("sqlite"))  # type: ignore[assignment] # FIXME CoP
+        self.engine = MockSaEngine("sqlite")  # type: ignore[assignment] # FIXME CoP
         self._create_temp_table = create_temp_table
-        self._connection = MockConnection()
+        self._connection = MockConnection()  # type: ignore[assignment] # FIXME CoP
 
-        self._batch_manager = None
+        self._batch_manager = None  # type: ignore[assignment] # FIXME CoP
 
+    @override
     def get_compute_domain(
         self,
         domain_kwargs: dict,
